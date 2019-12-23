9d80147d725681a8903aa633d5f798b8112e4279
Chris Miller <chris.miller8823@gmail.com>
2019-12-23 23:35:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    puts "Fizz"
+  if num % 3 == 0 && num % 5 == 0
+    return "Fizzbuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
   else
-    nil
+    retrun nil
   end
 end
